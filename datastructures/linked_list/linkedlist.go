a48a1bcdaee6e897407e069257855682e60d4cc7
Aleksander Nagaj <48178456+A-NGJ@users.noreply.github.com>
2022-07-26 12:19:37 +0000

    linked list ready

--- datastructures/linked_list/linkedlist.go
+++ datastructures/linked_list/linkedlist.go
@@ -1,2 +1,92 @@
 package linkedlist
 
+import "fmt"
+
+type node struct {
+	value interface{}
+	next  *node
+}
+
+type LinkedList struct {
+	head   *node
+	tail   *node
+	Length int
+}
+
+func New() LinkedList {
+	return LinkedList{}
+}
+
+func (ll *LinkedList) Append(val interface{}) {
+	newNode := node{value: val}
+	if ll.head == nil {
+		ll.head = &newNode
+		ll.tail = ll.head
+	} else {
+		ll.tail.next = &newNode
+		ll.tail = &newNode
+	}
+	ll.Length += 1
+	return
+}
+
+func (ll *LinkedList) Prepend(val interface{}) {
+	newNode := node{value: val, next: ll.head}
+	ll.head = &newNode
+	ll.Length += 1
+}
+
+func (ll *LinkedList) Insert(position int, val interface{}) error {
+	if position < 0 {
+		return fmt.Errorf("position cannot be negative")
+	}
+
+	if position >= ll.Length {
+		ll.Append(val)
+		ll.Length += 1
+		return nil
+	}
+
+	if position == 0 {
+		ll.Prepend(val)
+		ll.Length += 1
+		return nil
+	}
+
+	before := ll.traverseToPosition(position - 1)
+	newNode := node{value: val, next: before.next}
+	before.next = &newNode
+
+	return nil
+}
+
+func (ll *LinkedList) Remove(position int) error {
+	if position < 0 || position > ll.Length {
+		return fmt.Errorf("position %d out of range", position)
+	}
+
+	before := ll.traverseToPosition(position - 1)
+	before.next = before.next.next
+	ll.Length--
+	return nil
+}
+
+func (ll LinkedList) traverseToPosition(position int) *node {
+	var i int
+	curr := ll.head
+	for i != position {
+		curr = curr.next
+		i++
+	}
+	return curr
+}
+
+func (ll *LinkedList) Print() {
+	values := []interface{}{}
+	curr := ll.head
+	for curr != nil {
+		values = append(values, curr.value)
+		curr = curr.next
+	}
+	fmt.Println(values)
+}
--- main.go
+++ main.go
@@ -3,37 +3,56 @@ package main
 import (
 	"fmt"
 	// "main/datastructures/array"
-  "main/datastructures/hash_table"
+	// "main/datastructures/hash_table"
+	"main/datastructures/linked_list"
 )
 
 func main() {
+	// ========== Array ===========
 	// arr := array.New()
- //  arr.Push("a")
- //  arr.Push("b")
- //  arr.Push("c")
- //  arr.Delete(1)
- //  fmt.Printf("%+v\n", arr)
+	//  arr.Push("a")
+	//  arr.Push("b")
+	//  arr.Push("c")
+	//  arr.Delete(1)
+	//  fmt.Printf("%+v\n", arr)
 
- //  fmt.Println(array.Reverse("abcdef"))
+	//  fmt.Println(array.Reverse("abcdef"))
 
- //  arr1 := []int{0, 3, 4, 31}
- //  arr2 := []int{4, 6, 30}
- //  fmt.Println(array.MergeSorted(arr1, arr2))
+	//  arr1 := []int{0, 3, 4, 31}
+	//  arr2 := []int{4, 6, 30}
+	//  fmt.Println(array.MergeSorted(arr1, arr2))
 
-  hashTable := hash_table.New(50)
-  hashTable.Set("Mandag", 1)
-  hashTable.Set("Mandag", 3)
-  hashTable.Set("Tirsdag", 88)
-  _, err := hashTable.Get("B")
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println(hashTable)
-  fmt.Println(hashTable.Keys())
-  a1 := []int{2, 5, 1, 2, 3, 5, 1, 2, 4}
-  a2 := []int{2, 1, 1, 2, 3, 5, 1, 2, 4}
-  a3 := []int{2, 3, 4, 5}
-  fmt.Println(hash_table.FirstRecurringCharacter(a1))
-  fmt.Println(hash_table.FirstRecurringCharacter(a2))
-  fmt.Println(hash_table.FirstRecurringCharacter(a3))
+	// ============== Hash Table ==============
+	// hashTable := hash_table.New(50)
+	// hashTable.Set("Mandag", 1)
+	// hashTable.Set("Mandag", 3)
+	// hashTable.Set("Tirsdag", 88)
+	// _, err := hashTable.Get("B")
+	// if err != nil {
+	//   fmt.Println(err)
+	// }
+	// fmt.Println(hashTable)
+	// fmt.Println(hashTable.Keys())
+	// a1 := []int{2, 5, 1, 2, 3, 5, 1, 2, 4}
+	// a2 := []int{2, 1, 1, 2, 3, 5, 1, 2, 4}
+	// a3 := []int{2, 3, 4, 5}
+	// fmt.Println(hash_table.FirstRecurringCharacter(a1))
+	// fmt.Println(hash_table.FirstRecurringCharacter(a2))
+	// fmt.Println(hash_table.FirstRecurringCharacter(a3))
+
+	// =============== Linked list ==============
+	linkedList := linkedlist.New()
+	linkedList.Append(1)
+	linkedList.Append(2)
+	linkedList.Prepend(3)
+	linkedList.Append(4)
+	linkedList.Insert(3, 45)
+	linkedList.Insert(1, 45)
+	linkedList.Print()
+	linkedList.Remove(2)
+	linkedList.Print()
+	err := linkedList.Remove(33)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
